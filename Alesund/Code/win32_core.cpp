2ff7791af64d4cb0a29e1043390567758c84e689
Diego <36131808+Diegovano@users.noreply.github.com>
2019-09-03 23:14:06 +0100

    Started writing win32 code

--- Alesund/Code/win32_core.cpp
+++ Alesund/Code/win32_core.cpp
@@ -1,17 +1,46 @@
-/* The start of this amazing project */
+/* The beginning of a journey */
 
+#include <cassert>
 #include <stdint.h>
 #include <windows.h>
-#include <GL/gl.h>
-#include <glext.h>
 
-#include <stdio.h>
-
-int32_t main(int32_t argc, char *argv[])
+LRESULT CALLBACK window_callback(HWND window_handle, UINT message, WPARAM wparam, LPARAM lparam)
 {
-    printf("Hello World!\n");
+	return 0;
+}
 
-    getchar();
-    
-    return(0);
+int WinMain(HINSTANCE hinstance, HINSTANCE previous_instance, LPSTR cmdline, int showcmd)
+{
+	const char *window_class_name = "alesund_window_class";
+	const char *window_name = "Alesund";
+
+	WNDCLASS window_class = {};
+	window_class.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+	window_class.hInstance = hinstance;
+	window_class.lpszClassName = window_class_name;
+	window_class.lpfnWndProc = window_callback;
+
+	assert(RegisterClass(&window_class));
+
+	HWND window = CreateWindowEx(0,
+		window_class_name,
+		window_name,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hinstance,
+		NULL);
+
+	ShowWindow(window, showcmd);
+
+	while(true)
+	{
+		
+	}
+
+	return 0;
 }
